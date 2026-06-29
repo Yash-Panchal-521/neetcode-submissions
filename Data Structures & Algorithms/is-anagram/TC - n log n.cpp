3cd8c62ab62e7e2cf8acc4c5e336b59dd2120c31
Yash Panchal <yp06446@gmail.com>
2026-06-29 15:07:03 +0530

    Implement isAnagram using sorting method
    Added a new implementation for checking anagrams using sorting.

--- Data Structures & Algorithms/is-anagram/TC - n log n.cpp
+++ Data Structures & Algorithms/is-anagram/TC - n log n.cpp
@@ -1,13 +1,17 @@
 class Solution {
 public:
     bool isAnagram(string s, string t) {
+
+        // Sorting string
         sort(s.begin(), s.end());
         sort(t.begin(), t.end());
 
+        // Length validation
         if(s.length() != t.length()) {
             return false;
         }
-
+        
+        // Checking character one by one
         for(int i = 0; i < s.length(); i++) {
             if(s[i] != t[i]) {
                 return false;
