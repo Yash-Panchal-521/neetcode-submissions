ab72939a5cfe4b7f11aa14e79fcb45b12bc9b29f
Yash Panchal <yp06446@gmail.com>
2026-06-29 15:05:04 +0530

    Enhance comments on duplicate detection method
    Added comments explaining time complexities of map and unordered map.

--- Data Structures & Algorithms/duplicate-integer/TC - n log n.cpp
+++ Data Structures & Algorithms/duplicate-integer/TC - n log n.cpp
@@ -1,6 +1,8 @@
 class Solution {
 public:
     bool hasDuplicate(vector<int>& nums) {
+        // Map -> n Log n for insert / update
+        // Unordered Map -> n for insert / update
         map<int, int> mp; 
         for(auto i: nums){
             if(mp[i] > 0) {
